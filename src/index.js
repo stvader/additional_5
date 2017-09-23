41aea07fe84b875a09d65f4667b7e74ee97d10ea
Vadim St <vadim_st_13@mail.ru>
2017-09-23 19:06:30 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,78 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
+	let configObj = {};
+	//let strArr = str.split('');
+	let stack = [];
+
 
 	if (str.length%2) return false;
 
-	let strArr = str.split('');
-	let bracketsOpen = []; 
-	let bracketsClose = [];
-
-	function searchFirstClosedBracket(beginPos, arr, arrClose, arrOpen) {
-		for (let i=beginPos+1; i<arr.length; i++) {
-			if (arrClose.indexOf(arr[i]) !== -1) {
-				if(checkBracketsEqual(arr[i], arrOpen, arrClose)
-					&& arr[i] !== arr[beginPos]) {
-					return searchFirstClosedBracket(i, arr, arrClose, arrOpen);
-				}
-
-				return i;
-			}
+	for (let i=0; i<bracketsConfig.length; i++) {
+		let bracketPair = bracketsConfig[i];
+		let openedBracket = bracketPair[0];
+		configObj[openedBracket] = {};
+		configObj[openedBracket].closedBracket = bracketPair[1];
+		if (openedBracket === bracketPair[1]) {
+			configObj[openedBracket].isEqualBrackets = true;
+			configObj[openedBracket].isInStack = false;
+		} else {
+			configObj[openedBracket].isEqualBrackets = false;
 		}
 	}
 
-	function checkBracketsPair(openBracket, closeBracket, arr) {
-		for (let i=0; i<arr.length; i++) {
-			if (arr[i][0] === openBracket && arr[i][1] === closeBracket) return true;		
+	for (let i=0; i<str.length; i++) {
+		let currentBracket = str[i];
+		let lastBracket;
+		let sameFlag = false;
+
+		if (configObj.hasOwnProperty(currentBracket)
+			&& (!configObj[currentBracket].isEqualBrackets 
+				|| (configObj[currentBracket].isEqualBrackets 
+					&& !configObj[currentBracket].isInStack))) {
+			stack.push(currentBracket);
+		    if (configObj[currentBracket].isEqualBrackets) {
+		    	configObj[currentBracket].isInStack = !configObj[currentBracket].isInStack;
+		    }
+			
+		} else {
+			if (stack.length === 0) return false;
+
+			lastBracket = stack[stack.length-1];
+			if(currentBracket !== configObj[lastBracket].closedBracket) return false;
+			if (configObj[lastBracket].isEqualBrackets) {
+		    	configObj[lastBracket].isInStack = !configObj[lastBracket].isInStack;
+		    }
+			stack.pop();
 		}
-		return false;
 	}
 
-	function checkBracketsEqual(closeBreak, arrOpen, arrClose) {
-		let posCloseArr = arrClose.indexOf(closeBreak);
-		if (closeBreak === arrOpen[posCloseArr]) return true;
-		return false;
-	}
-
-	function getCheckedBracket(current, start) {//arr
-		let numberCloseBracket = 2*current-start-1; 
-		let checkedBracket = strArr[numberCloseBracket];
-
-		/*if(bracketsOpen.indexOf(checkedBracket)) {
-			current = numberCloseBracket;////////
-			return getCheckedBracket(current, start);
-		}*/
-
-		return numberCloseBracket;
-	}
-
-	bracketsConfig.forEach((item) => {
-		bracketsOpen.push(item[0]);
-		bracketsClose.push(item[1]);
-	});
-
-	for (let i=0; i<strArr.length; i++) {
-		let beginBracket = strArr[i];
-		if (beginBracket === null) continue;
-		if (bracketsOpen.indexOf(beginBracket) === -1) return false;
-
-		let currentClose = searchFirstClosedBracket(i, strArr, bracketsClose, bracketsOpen);
-		/////
-
-		let checkedNum = getCheckedBracket(currentClose, i);
-		let checkedBracket = strArr[checkedNum];
-
-		
-		//////
-
-		if (!checkBracketsPair(beginBracket, checkedBracket, bracketsConfig)) return false;
-
-		strArr[i] = null;
-		strArr[2*currentClose-i-1] = null;
-	}
-
-	strArr.forEach((item) => {
-		if (item !== null) return false;
-	});
-
-	return true;  
+	if (stack.length !== 0) return false;	
   
+	return true;  
 }
